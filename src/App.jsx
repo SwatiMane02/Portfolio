425d2c8048b5363a9a17d4a73b39dc5a64c5d823
SwatiMane02 <swatidmane02@gmail.com>
2026-04-18 20:54:50 +0530

    setup app entry and routing structure

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from 'react';
+import Lenis from 'lenis';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import CursorEffect from './components/CursorEffect';
+import Home from './pages/Home';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
+    lenisRef.current = lenis;
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove((time) => lenis.raf(time * 1000));
+    };
+  }, []);
+
+  return (
+    <div className="bg-[#0b0b0b] text-white selection:bg-brand selection:bg-opacity-30">
+      <CursorEffect />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
